6942b43049ee1338188f6d37a3a718ddd5594796
Teta <tetref@users.noreply.github.com>
2026-01-14 20:59:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/cacheforge/main.go
@@ -0,0 +1,23 @@
+// cmd/cacheforge/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "cacheforge/internal/cacheforge"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := cacheforge.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
